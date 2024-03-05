930f99cf929a5007ba827bbfc2e0cee7e34c0364
Mersio <mersiocarneiro87@gmail.com>
2024-03-05 19:04:12 -0300

    update

--- full-stack-javascript/javascript/javascript-modulo-06/exercicio-04/index.js
+++ full-stack-javascript/javascript/javascript-modulo-06/exercicio-04/index.js
@@ -1,4 +1,4 @@
-function renderTrasition(transactionsData) {
+function renderTransaction(transactionsData) {
   const article = document.createElement("article");
   article.classList.add("transactions");
   article.id = `transactions-${transactionsData.id}`;
@@ -15,7 +15,29 @@ function renderTrasition(transactionsData) {
   description.classList.add("transactions-description");
   description.innerText = transactionsData.description;
 
+  const containerBtn = document.createElement("div");
+  containerBtn.classList.add("container-btn");
+
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-btn");
+  editBtn.type = "button";
+  editBtn.innerText = "Editar";
+  editBtn.addEventListener("click", () => {
+    const inputHidden = document.createElement("input");
+    inputHidden.id = "id";
+    inputHidden.type = "hidden";
+
+    const form = document.querySelector("#form");
+    form.appendChild(inputHidden);
+
+    document.querySelector("#id").value = transactionsData.id;
+    document.querySelector("#name").value = transactionsData.name;
+    document.querySelector("#value").value = transactionsData.value;
+    document.querySelector("#description").value = transactionsData.description;
+  });
+
   const deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn-delete");
   deleteBtn.type = "button";
   deleteBtn.innerText = "Deletar";
   deleteBtn.addEventListener("click", async () => {
@@ -24,13 +46,15 @@ function renderTrasition(transactionsData) {
     });
   });
 
-  article.append(name, value, description, deleteBtn);
+  containerBtn.append(editBtn, deleteBtn);
+
+  article.append(name, value, description, containerBtn);
   document.querySelector("#transactions").appendChild(article);
 }
 
 function renderTotal(transactions) {
-  const total = transactions.reduce((acc, transactions) => {
-    return acc + Number(transactions.value);
+  const total = transactions.reduce((acc, transaction) => {
+    return acc + Number(transaction.value);
   }, 0);
 
   const totalSection = document.querySelector("#total");
@@ -42,19 +66,18 @@ function renderTotal(transactions) {
 }
 
 let transactionsArray = [];
+console.log(transactionsArray);
 
 async function fecthTransactions() {
   const transactions = await fetch("http://localhost:3000/transactions").then(
     (res) => res.json()
   );
 
-  transactions.map((transactions) => renderTrasition(transactions));
+  transactions.map((transaction) => renderTransaction(transaction));
   renderTotal(transactions);
 
-  transactionsArray.push(transactions);
+  transactionsArray.push(...transactions);
 }
-
-console.log(transactionsArray);
 
 // OBS: DOMContentLoaded é quando termina de carregar o conteúdo no DOM
 document.addEventListener("DOMContentLoaded", () => {
@@ -67,20 +90,45 @@ form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
   const transactionsData = {
+    id: document.querySelector("#id")?.value,
     name: document.querySelector("#name").value,
     value: document.querySelector("#value").value,
     description: document.querySelector("#description").value,
   };
 
-  const response = await fetch("http://localhost:3000/transactions", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(transactionsData),
-  });
+  if (transactionsData.id) {
+    const response = await fetch(
+      `http://localhost:3000/transactions/${transactionsData.id}`,
+      {
+        method: "PUT",
+        body: JSON.stringify(transactionsData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-  const savedTransactions = await response.json();
-  form.reset();
-  renderTrasition(savedTransactions);
+    const transaction = await response.json();
+    const indexToRemove = transactionsArray.findIndex(
+      (t) => t.id === transactionsData.id
+    );
+    transactionsArray.splice(indexToRemove, 1, transaction);
+    document.querySelector(`#transactions-${transactionsData.id}`).remove();
+    renderTransaction(transactionsArray);
+  } else {
+    const response = await fetch("http://localhost:3000/transactions", {
+      method: "POST",
+      body: JSON.stringify(transactionsData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const savedTransactions = await response.json();
+    transactionsArray.push(savedTransactions);
+    renderTransaction(savedTransactions);
+  }
+
+  // form.reset();
+  e.target.reset();
 });
